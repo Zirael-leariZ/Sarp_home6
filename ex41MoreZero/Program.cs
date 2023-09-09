5cbd31ccbd3edb5c83909564d948941f173843ec
Oleksii Ishchenko <optionalzirelleariz>
2023-09-09 05:12:30 -0600

    initial commit

---
+++ ex41MoreZero/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] AnyLength()
+{
+    int i = 0;
+   List<int> line = new List<int>();
+
+    while(true)
+    {
+        Console.WriteLine($"Choose the {i + 1} digit or press 'q' to quit");
+        string input = Console.ReadLine();
+
+        if (input == "q")
+        {
+            break;
+        }
+        else if (input != "q")
+        {
+            int num = Convert.ToInt32(input);
+            line.Add(num);
+            i++;
+        }
+    }
+    int[] intArray = line.ToArray();
+    return intArray;
+}
+
+int MoreThenZero(int[] line)
+{
+    int count = 0;
+    for(int i = 0; i < line.Length; i++)
+    {
+        if (line[i] >= 0)
+        count++;
+    }
+    return count;
+}
+
+int[] array = AnyLength();
+Console.WriteLine(string.Join(", ", array));
+
+int result = MoreThenZero(array);
+Console.WriteLine(result);
---
+++ ex43WhereCross/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+/*  y = k1 * x + b1
+    y = k2 * x + b2
+
+    y - y = k1 * x + b1 - (k2 * x + b2)
+    y = k2 * x + b2
+    
+    k2*x - k1*x = b1 - b2
+    x(k2-k1) = b1 - b2
+    
+    x = (b1 - b2) / (k2-k1)
+    y = k2*(b1 - b2) / (k2-k1) + b2*/
+
+
+using System.Runtime.Intrinsics.X86;
+using System.Security.Cryptography.X509Certificates;
+
+static void WhereLinesCross(out double x, out double y)
+{
+    Console.Write("Enter the value for k1: ");
+    double k1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the value for b1: ");
+    double b1 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the value for k2: ");
+    double k2 = Convert.ToDouble(Console.ReadLine());
+
+    Console.Write("Enter the value for b2: ");
+    double b2 = Convert.ToDouble(Console.ReadLine());
+
+    x = (b1 - b2) / (k2 - k1);
+    y = k1 * x + b1;
+}
+
+double x , y;
+WhereLinesCross(out x, out y);
+Console.Write($"{x}; {y}");
+
